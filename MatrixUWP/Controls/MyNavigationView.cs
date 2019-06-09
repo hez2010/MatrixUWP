a9d96c9d8bdf268b8966e04ba626b3d6cbb9094d
Steven He <hez2010@outlook.com>
2019-06-10 03:56:40 +0800

    Add project files.

---
+++ MatrixUWP/Controls/MyNavigationView.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+
+// The Templated Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234235
+
+namespace MatrixUWP.Controls
+{
+    public sealed class MyNavigationView : NavigationView
+    {
+        public Visibility HeaderVisibility { get; set; }
+        public static readonly DependencyProperty HeaderVisibilityProperty = DependencyProperty.Register(nameof(HeaderVisibility), typeof(Visibility), typeof(NavigationView), new PropertyMetadata(Visibility.Visible));
+
+        public MyNavigationView()
+        {
+            this.DefaultStyleKey = typeof(MyNavigationView);
+        }
+    }
+}
---
+++ MatrixUWP/Pages/Layout.xaml.cs
@@ -0,0 +1,80 @@
+﻿using MatrixUWP.Controls;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Resources;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace MatrixUWP.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Layout : Page
+    {
+        public Layout()
+        {
+            this.InitializeComponent();
+
+            Windows.ApplicationModel.Core.CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = true;
+            var titleBar = Windows.UI.ViewManagement.ApplicationView.GetForCurrentView().TitleBar;
+            titleBar.ButtonBackgroundColor = Colors.Transparent;
+            titleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+
+            NaviMenu.SelectedItem = HomePage;
+        }
+
+        private int lastSelectedItemIndex = 0;
+        private void NavigateToPage(object itemObject, bool isSettingsPage, NavigationViewPaneDisplayMode paneDisplayMode)
+        {
+            if (!(itemObject is NavigationViewItem item)) return;
+            var index = NaviMenu.MenuItems.IndexOf(item);
+            if (isSettingsPage)
+            {
+                item.Content = App.CultureResource.GetString("NaviMenu_Item_Settings/Content");
+                index = NaviMenu.MenuItems.Count;
+            }
+
+            NavigationTransitionInfo transition;
+            if (paneDisplayMode == NavigationViewPaneDisplayMode.Top)
+            {
+                transition = new SlideNavigationTransitionInfo
+                {
+                    Effect = lastSelectedItemIndex <= index ? SlideNavigationTransitionEffect.FromRight : SlideNavigationTransitionEffect.FromLeft
+                };
+            }
+            else transition = new DrillInNavigationTransitionInfo();
+            lastSelectedItemIndex = index;
+
+            NaviContent.Navigate((item.Name, isSettingsPage) switch
+            {
+                (_, true) => typeof(Pages.Settings),
+                ("HomeNaviPage", _) => typeof(Pages.Generic.Home),
+                _ => typeof(Pages.Generic.Home)
+            }, null, transition);
+        }
+
+        private void NaviMenu_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            NavigateToPage(args.InvokedItem, args.IsSettingsInvoked, sender.PaneDisplayMode);
+        }
+        private void NaviMenu_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            NavigateToPage(args.SelectedItem, args.IsSettingsSelected, sender.PaneDisplayMode);
+        }
+    }
+}
