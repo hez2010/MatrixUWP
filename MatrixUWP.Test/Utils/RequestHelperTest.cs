949a71bee248c4f81e36871f6e5963bd97c46034
Steven He <hez2010@outlook.com>
2019-06-10 13:11:32 +0800

    Add test, request helper

---
+++ MatrixUWP.Test/Utils/RequestHelperTest.cs
@@ -0,0 +1,25 @@
+﻿using MatrixUWP.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage.Streams;
+
+namespace MatrixUWP.Test.Utils
+{
+    [TestClass]
+    public class RequestHelperTest
+    {
+        [TestMethod]
+        public async Task GetAsyncTest()
+        {
+            var stream = new InMemoryRandomAccessStream();
+            var result = await RequestHelper.GetAsync("/", null, stream);
+            Assert.IsTrue(result);
+
+            return;
+        }
+    }
+}
