0bd5f66672a9058e9744c4f9867a6d9ebe391118
Steven He <hez2010@outlook.com>
2019-06-19 12:19:55 +0800

    fixes

--- MatrixUWP/Extensions/ResponseExtensions.cs
+++ MatrixUWP/Extensions/ResponseExtensions.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+#if FXJSON
 using System.Text.Json.Serialization;
+#else
+using Newtonsoft.Json;
+#endif
 using System.Threading.Tasks;
 using Windows.Storage.Streams;
 using Windows.Web.Http;
@@ -8,18 +12,21 @@ namespace MatrixUWP.Extensions
 {
     static class ResponseExtensions
     {
-        private static readonly JsonSerializerOptions options = new JsonSerializerOptions
-        {
-            AllowTrailingCommas = true,
-            IgnoreReadOnlyProperties = true,
-        };
-
+#if FXJSON
         public static async ValueTask<T> JsonAsync<T>(this ValueTask<HttpResponseMessage> response)
         {
             var result = await response;
             var json = await result.Content.ReadAsStringAsync();
-            return JsonSerializer.Parse<T>(json, options);
+            return JsonSerializer.Parse<T>(json);
         }
+#else
+        public static async ValueTask<T> JsonAsync<T>(this ValueTask<HttpResponseMessage> response)
+        {
+            var result = await response;
+            var json = await result.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+#endif
 
         public static async ValueTask<string> TextAsync(this ValueTask<HttpResponseMessage> response)
         {
